05a355126bb646233337709b3218afb2a3cafceb
Soliman Halaby <soliman.al-halaby@hetic.net>
2022-03-04 03:57:11 +0200

    :rocket: Mates redirection

--- scripts/redirection.js
+++ scripts/redirection.js
@@ -1,3 +1,14 @@
+const matesLink = document.querySelectorAll(".detail a");
+
+matesLink.forEach((mateLink) => {
+  console.log(mateLink);
+  // console.log(mateLink.getAttribute("href"));
+  const mateUrl = mateLink.getAttribute("href");
+  mateLink.addEventListener("click", () => {
+    window.location.href = mateUrl;
+  });
+});
+
 const x = window.matchMedia("(max-width: 700px)");
 
 function mediaQuery() {
